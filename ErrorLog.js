a8ced4adfe620baf9c98509619978b0306bf5fe3
sheeban <sheeban@gooru.org>
2019-12-27 15:41:25 +0530

    Fixed syntax error

--- ErrorLog.js
+++ ErrorLog.js
@@ -2,7 +2,7 @@ var winston = require('winston');
 require('winston-daily-rotate-file');
 var config = require(process.env.CONFIG_FILE_PATH);
 var transport = new (winston.transports.DailyRotateFile)({
-	filename : 'config.appConfiguration.loggerFilePath',
+	filename : config.appConfiguration.loggerFilePath,
 	datePattern : 'nucleus_idp_error-yyyy-MM-dd.',
 	prepend : true,
 	level : 'error'
